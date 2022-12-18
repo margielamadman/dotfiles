4cc5d02e935a27f1f4e941bb606e369de4077745
m <39255541+margielamadman@users.noreply.github.com>
2022-12-17 18:46:57 -0800

    Set up file browser

--- nvim/.config/nvim/after/plugin/telescope.rc.lua
+++ nvim/.config/nvim/after/plugin/telescope.rc.lua
@@ -2,10 +2,20 @@ local status, telescope = pcall(require, 'telescope')
 if (not status) then return end
 
 -- Telescope Setup
-telescope.setup {}
+telescope.setup {
+  extensions = {
+    file_browser = {
+      theme = 'ivy',
+      -- disables netrw and use telescope-file-browser
+      hijack_netrw = true,
+    }
+  }
+}
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
-require('telescope').load_extension('fzf')
+telescope.load_extension('fzf')
+-- Loading file browser extension
+telescope.load_extension('file_browser')
 -- Example of how to pass opts if needed to builtin functions
 local mappings = {}
 mappings.curr_buf_fzf = function()
